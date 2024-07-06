e5ef98db5b63d1b4d246a80bdc1394ae0e055170
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2024-07-07 02:16:07 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -34,25 +34,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "─▷─────────"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "──▷────────"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "───▷───────"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "─────▷─────"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "──────▷────"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "───────▷───"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "────────▷──"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "────────▷──"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "─────────▷─"
     else:
-        bar = "—————————◉"
+        bar = "─────────▷─"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -61,28 +61,14 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="❤️‍🔥", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="👻", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🪫", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="♦️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-            ],
-        [
-            InlineKeyboardButton(text="🌼 ᴘʟᴀʏʟɪsᴛs", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(
-                text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING"
-            ),
-        ],
-        [
-            InlineKeyboardButton(text="💮︎ ᴄᴘᴀɴɴᴇʟ", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(
-                text="⛩️ Gʀᴏᴜᴘꨄ︎ ⛩️", url=SUPPORT_CHAT,
-            )
+         InlineKeyboardButton(text="", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -92,28 +78,14 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="❤️‍🔥", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="👻", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🪫", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="♦️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-            ],
-        [
-            InlineKeyboardButton(text="🌼 ᴘʟᴀʏʟɪsᴛs", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(
-                text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING"
-            ),
-        ],
-        [
-            InlineKeyboardButton(text="💮︎ ᴄᴘᴀɴɴᴇʟ", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(
-                text="⛩️ Gʀᴏᴜᴘꨄ︎ ⛩️", url=SUPPORT_CHAT,
-            )
+         InlineKeyboardButton(text="", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
